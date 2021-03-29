1a68c37223558a57994c22113ab04adea876607e
muhammadmadum <aku.malu211000@gmail.com>
2021-03-29 13:54:26 +0700

    Menambahkan dan membagi file view menjadi beberapa bagian

---
+++ application/views/partial/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html style="font-size: 16px;">
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="utf-8">
+    <meta name="keywords" content="In our Campus you can, Our Courses, Our Teachers, John Mcallister, Jane Doe, Mary Mgrayan, Hanna Kim, 44, +10 K, +47, 15, Our Blog, Our university, Course Registration">
+    <meta name="description" content="">
+    <meta name="page_type" content="np-template-header-footer-from-plugin">
+    <title>Home</title>
+    <link rel="stylesheet" href="<?= base_url('assets/css/') ?>nicepage.css" media="screen">
+    <link rel="stylesheet" href="<?= base_url('assets/css/') ?>Home.css" media="screen">
+    <script class="u-script" type="text/javascript" src="<?= base_url('assets/js/') ?>jquery-1.9.1.min.js" defer=""></script>
+    <script class="u-script" type="text/javascript" src="<?= base_url('assets/js/') ?>nicepage.js" defer=""></script>
+    <meta name="generator" content="Nicepage 3.10.4, nicepage.com">
+    <link id="u-theme-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+    <link id="u-page-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700">
+    <script type="application/ld+json">
+    {
+        "@context": "http://schema.org",
+        "@type": "Organization",
+        "name": "",
+        "url": "index.html",
+        "logo": "images/Logo-mahasiswa-coding.png",
+        "sameAs": []
+    }
+    </script>
+    <meta property="og:title" content="Home">
+    <meta property="og:type" content="website">
+    <meta name="theme-color" content="#73a3de">
+    <link rel="canonical" href="index.html">
+    <meta property="og:url" content="index.html">
+</head>
+<body class="u-body">
---
+++ application/views/partial/main_nav.php
@@ -0,0 +1,64 @@
+    <header class="u-clearfix u-header" id="sec-e20f">
+        <div class="u-clearfix u-sheet u-sheet-1">
+            <nav class="u-align-right u-menu u-menu-dropdown u-nav-spacing-25 u-offcanvas u-menu-1">
+                <div class="menu-collapse" style="font-size: 1rem; letter-spacing: 0px; text-transform: uppercase; font-weight: 700;">
+                    <a class="u-btn-text u-button-style u-custom-active-border-color u-custom-border u-custom-border-color u-custom-borders u-custom-hover-border-color u-custom-left-right-menu-spacing u-custom-padding-bottom u-custom-text-active-color u-custom-text-color u-custom-text-hover-color u-custom-top-bottom-menu-spacing u-nav-link" href="#" style="padding: 2px 0px; font-size: calc(1em + 4px);">
+                        <svg class="u-svg-link" preserveAspectRatio="xMidYMin slice" viewBox="0 0 302 302" style="undefined">
+                            <use xlink:href="#svg-7b92"></use>
+                        </svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="svg-7b92" x="0px" y="0px" viewBox="0 0 302 302" style="enable-background:new 0 0 302 302;" xml:space="preserve" class="u-svg-content">
+                            <g>
+                                <rect y="36" width="302" height="30"></rect>
+                                <rect y="236" width="302" height="30"></rect>
+                                <rect y="136" width="302" height="30"></rect>
+                            </g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                            <g></g>
+                        </svg>
+                    </a>
+                </div>
+                <div class="u-custom-menu u-nav-container">
+                    <ul class="u-custom-font u-nav u-spacing-30 u-text-font u-unstyled u-nav-1">
+                        <li class="u-nav-item"><a class="u-border-2 u-border-active-grey-15 u-border-hover-grey-40 u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-palette-1-base u-text-grey-90 u-text-hover-grey-90" style="padding: 10px 15px;">Beranda</a>
+                        </li>
+                        <li class="u-nav-item"><a class="u-border-2 u-border-active-grey-15 u-border-hover-grey-40 u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-palette-1-base u-text-grey-90 u-text-hover-grey-90" style="padding: 10px 15px;">Tutorial</a>
+                        </li>
+                        <li class="u-nav-item"><a class="u-border-2 u-border-active-grey-15 u-border-hover-grey-40 u-border-no-left u-border-no-right u-border-no-top u-button-style u-nav-link u-text-active-palette-1-base u-text-grey-90 u-text-hover-grey-90" style="padding: 10px 15px;">Tentang</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="u-custom-menu u-nav-container-collapse">
+                    <div class="u-align-center u-black u-container-style u-inner-container-layout u-opacity u-opacity-95 u-sidenav">
+                        <div class="u-sidenav-overflow">
+                            <div class="u-menu-close"></div>
+                            <ul class="u-align-center u-nav u-popupmenu-items u-unstyled u-nav-2">
+                                <li class="u-nav-item"><a class="u-button-style u-nav-link" style="padding: 10px 15px;">Beranda</a>
+                                </li>
+                                <li class="u-nav-item"><a class="u-button-style u-nav-link" style="padding: 10px 15px;">Tutorial</a>
+                                </li>
+                                <li class="u-nav-item"><a class="u-button-style u-nav-link" style="padding: 10px 15px;">Tentang</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="u-black u-menu-overlay u-opacity u-opacity-70"></div>
+                </div>
+            </nav>
+            <a href="#" class="u-image u-logo u-image-1" src="" data-image-width="2168" data-image-height="2168">
+                <img src="<?= base_url('assets/') ?>images/Logo-mahasiswa-coding.png" class="u-logo-image u-logo-image-1" data-image-width="97.3334">
+            </a>
+        </div>
+    </header>
